31e47902c3a636c6a978f775e778db2d9c33b44c
LonglifeHyun <44224844+LonglifeHyun@users.noreply.github.com>
2020-01-16 00:59:38 +0900

    First Version

---
+++ main_longlife.js
@@ -0,0 +1,12 @@
+function mOver(obj) {
+  obj.innerHTML = "My Real Name is Jang Soo."
+}
+
+function mOut(obj) {
+  obj.innerHTML = "My Name is Long Life."
+}
+
+function showPopup() {
+  window.open("lol_story.html", "lol_story",
+              "width=600, height=450, left=600, top=250");
+}
